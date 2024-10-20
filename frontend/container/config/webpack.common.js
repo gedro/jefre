65ce10ca3a3f937ad942038735c3cc4c2a69e5fc
Gabor Harsanyi <harsanyigabor@gmail.com>
2024-10-20 16:12:33 +0200

    test html webpack config

--- frontend/container/config/webpack.common.js
+++ frontend/container/config/webpack.common.js
@@ -19,6 +19,23 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      favicon: './public/favicon.ico',
+      //     <!--
+      //       manifest.json provides metadata used when your web app is installed on a
+      //       user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+      //     -->
+      manifest: './public/manifest.json',
+      meta: {
+        charset: 'utf-8',
+        viewport: 'width=device-width, initial-scale=1',
+        'theme-color': '#000000',
+      },
+      links: [
+        {
+          rel: 'apple-touch-icon',
+          href: 'public/apple-touch-icon.png',
+        },
+      ],
     }),
   ],
 };
