30ae6c70f07d679cf3d41b56d2b2fd43fd37cb68
Gabor Harsanyi <harsanyigabor@gmail.com>
2024-10-23 19:11:07 +0200

    improve code reuse

--- frontend/container/src/components/MarketingApp.js
+++ frontend/container/src/components/MarketingApp.js
@@ -1,27 +1,6 @@
 import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import doMount from './MountUtil';
 
 export default ({ appContext, onAppContextChanged }) => {
-  const ref = useRef(null);
-  const history = useHistory();
-
-  useEffect(() => {
-    const { onParentNavigate } = mount(ref.current, {
-      initialPath: history.location.pathname,
-      onNavigate: ({ pathname: nextPathname }) => {
-        const { pathname } = history.location;
-
-        if (pathname !== nextPathname) {
-          history.push(nextPathname);
-        }
-      },
-      appContext: appContext,
-      onAppContextChanged: onAppContextChanged
-    });
-
-    history.listen(onParentNavigate);
-  }, []);
-
-  return <div ref={ref} />;
+  return doMount(mount, { appContext, onAppContextChanged });
 };
