fe7fda4ff83f9829f57b215f980e9720bb60879c
Gabor Harsanyi <harsanyigabor@gmail.com>
2024-10-24 10:48:54 +0200

    cleanup

--- frontend/services/api/src/App.js
+++ frontend/services/api/src/App.js
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import api from './services/api';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ap',
+});
 
 export default ({ appContext, onAppContextChanged, history }) => {
 
@@ -10,6 +15,8 @@ export default ({ appContext, onAppContextChanged, history }) => {
   }, [appContext.apiUrl]);
 
   return (
+    <StylesProvider generateClassName={generateClassName}>
     <div />
+    </StylesProvider>
   );
 };
