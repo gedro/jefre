08047e1875206457c9278cb3ace14fe58afc691a
Gabor Harsanyi <harsanyigabor@gmail.com>
2024-11-17 18:45:15 +0100

    cleanup

--- frontend/auth/src/components/SocialButtons.js
+++ frontend/auth/src/components/SocialButtons.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import Divider from "@mui/material/Divider";
