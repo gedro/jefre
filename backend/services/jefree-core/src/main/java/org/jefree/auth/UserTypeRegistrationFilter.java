eaa171e8702790b945a24a052040f77644fc4050
Gabor Harsanyi <harsanyigabor@gmail.com>
2024-12-07 00:50:46 +0100

    added debug logs

--- backend/services/jefree-core/src/main/java/org/jefree/auth/UserTypeRegistrationFilter.java
+++ backend/services/jefree-core/src/main/java/org/jefree/auth/UserTypeRegistrationFilter.java
@@ -4,11 +4,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.web.util.UrlUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
 public class UserTypeRegistrationFilter extends OncePerRequestFilter {
+
+  private static final Logger logger = LoggerFactory.getLogger(UserTypeRegistrationFilter.class);
 
   /**
    * Same contract as for {@code doFilter}, but guaranteed to be
@@ -26,6 +33,15 @@ public class UserTypeRegistrationFilter extends OncePerRequestFilter {
     final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain
   ) throws ServletException, IOException {
     try {
+      UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(UrlUtils.buildFullRequestUrl(request))
+        .replacePath(request.getContextPath())
+        .replaceQuery(null)
+        .fragment(null)
+        .build();
+      logger.error("****************************************************************************{}", uriComponents);
+      logger.error("****************************************************************************{}", request.getHeader("X-Forwarded-Proto"));
+      logger.error("****************************************************************************{}", request.getHeader("X-Forwarded-Port"));
+
       final String registrationType = request.getParameter("registrationType");
       if(registrationType != null) {
         request.getSession().setAttribute("registrationType", registrationType);
