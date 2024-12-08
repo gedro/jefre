278d3493d990be02ae4096b88e27d19f3d330e3f
Gabor Harsanyi <harsanyigabor@gmail.com>
2024-12-08 22:54:40 +0100

    Added Editor shared component

--- frontend/components/config/webpack.common.js
+++ frontend/components/config/webpack.common.js
@@ -14,6 +14,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss|\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
     ],
   },
   plugins: [
---
+++ frontend/components/src/components/Editor.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
+const useStyles = makeStyles((theme) => ({
+  com_editor: {
+    height: '100%',
+  },
+}));
+
+const editorToolbarOptions = [
+  [{ 'font': [] }],
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  // [{ size: [ 'small', false, 'large', 'huge' ]}],
+  ['bold', 'italic', 'underline', 'strike'],
+  [{ 'align': [] }, { 'color': [] }, { 'background': [] }],
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+  [{ 'indent': '-1'}, { 'indent': '+1' }],
+  ['blockquote'],
+  ['link', 'image'],
+  [{ 'script': 'sub'}, { 'script': 'super' }],
+
+  ['clean']
+];
+
+export default function Editor({ id, value, onChange, readOnly }) {
+  const classes = useStyles();
+
+  return (
+    <ReactQuill
+      id={id}
+      bounds={'.re_new_job_desc_div'}
+      className={classes.com_editor}
+      value={value}
+      onChange={onChange}
+      modules={{
+        toolbar: editorToolbarOptions,
+      }}
+      readOnly={readOnly}
+    />
+  );
+}
