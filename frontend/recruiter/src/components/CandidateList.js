8dc1bfc54e4762a9bd9a52bfe6b961b30ce35c13
Gabor Harsanyi <harsanyigabor@gmail.com>
2024-12-20 17:52:51 +0100

    show different types on suggested candidate list if they match with the job

--- frontend/recruiter/src/components/CandidateList.js
+++ frontend/recruiter/src/components/CandidateList.js
@@ -2,12 +2,16 @@ import React, { Fragment, useState, useEffect } from "react";
 import {Link, useParams} from "react-router-dom";
 import toast from "react-hot-toast";
 
+import workTypes from /*webpackIgnore: true*/ '../../../utils/workTypes';
+import jobTypes from /*webpackIgnore: true*/ '../../../utils/jobTypes';
+import experienceLevels from /*webpackIgnore: true*/ '../../../utils/experienceLevels';
+
 import Loader from 'components/Loader';
 
 export default function CandidateList({ classes, appContext, onAppContextChanged, history }) {
   const { jobId } = useParams();
   const [loading, setLoading] = useState(false);
-  const [jobTitle, setJobTitle] = useState("");
+  const [job, setJob] = useState("");
   const [candidates, setCandidates] = useState([]);
 
   useEffect(() => {
@@ -15,7 +19,7 @@ export default function CandidateList({ classes, appContext, onAppContextChanged
       toast.error("Error fetching " + (jobId ? "suggested" : "favorite") + " candidates", err);
     });
     if(jobId) {
-      fetchJobTitle(jobId).catch((err) => {
+      fetchJob(jobId).catch((err) => {
         toast.error("Error fetching job title", err);
       });
     }
@@ -35,14 +39,35 @@ export default function CandidateList({ classes, appContext, onAppContextChanged
     }
   };
 
-  const fetchJobTitle = async (jobId) => {
+  const fetchJob = async (jobId) => {
     try {
       setLoading(true);
       const response = await appContext.api.get(`/jobs/${jobId}`);
-      setJobTitle(response?.data?.title);
+      setJob(response?.data);
     } finally {
       setLoading(false);
     }
+  };
+
+  const transformEnum = (enums, value) => {
+    return enums.find((item) => item.value === value)?.label;
+  };
+
+  const getTags = (job, candidate) => {
+    if(!job || !candidate) return [];
+
+    const tags = [];
+    candidate?.workTypes?.forEach((workType) => {
+      if(job?.workType === workType) tags.push(transformEnum(workTypes, workType));
+    });
+    candidate?.jobTypes?.forEach((jobType) => {
+      if(job?.jobType === jobType) tags.push(transformEnum(jobTypes, jobType));
+    });
+    candidate?.experienceLevels?.forEach((experienceLevel) => {
+      if(job?.experienceLevel === experienceLevel) tags.push(transformEnum(experienceLevels, experienceLevel));
+    });
+
+    return tags;
   };
 
   return (
@@ -50,7 +75,7 @@ export default function CandidateList({ classes, appContext, onAppContextChanged
       {loading ? ( <Loader /> ) : (
         <div className={classes.re_recruiter}>
           <h1>{jobId ? "Suggested Candidates" : "Favorite Candidates"}</h1>
-          {jobId && jobTitle && (<h2>({jobTitle})</h2>)}
+          {jobId && job?.title && (<h2>({job?.title})</h2>)}
           <div className={classes.re_recruiter_candidates_container}>
             {candidates.map((candidate) => (
               <div key={candidate.id} className={classes.re_recruiter_candidates_card}>
@@ -58,6 +83,11 @@ export default function CandidateList({ classes, appContext, onAppContextChanged
                   {candidate?.displayName}
                   <hr className={classes.re_new_job_hr}/>
                 </h2>}
+                <div className={classes.re_recruiter_jobs_dotted_container}>
+                  {getTags(job, candidate).map((tag) => (
+                    <div key={tag} className={classes.re_recruiter_jobs_dotted}>{tag}</div>
+                  ))}
+                </div>
                 <p><strong>Match: </strong>{Math.floor(Math.random() * 100)}%</p>
                 <div className={classes.re_recruiter_candidates_button_div}>
                   <Link to={`/recruiter/candidates/${candidate.id}`} className={classes.re_recruiter_jobs_link}>
